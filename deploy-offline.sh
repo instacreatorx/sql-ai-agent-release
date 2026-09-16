69766cdb65b2c3ce7dbe8a05ff063e1f056e933a
instacreatorx <insta.creator.x@gmail.com>
2026-09-16 20:44:54 +0330

    Add Docker apt repository installation steps

--- deploy-offline.sh
+++ deploy-offline.sh
@@ -1,5 +1,33 @@
 #!/usr/bin/env bash
 
+# ----------------------------------------------------
+# DOCKER INSTALLATION
+# ----------------------------------------------------
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+
+# Install the Docker packages:
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Verify that Docker is running:
+sudo systemctl status docker
+
 # Strict configurations to capture errors instantly within our FSM
 set -eo pipefail
 
